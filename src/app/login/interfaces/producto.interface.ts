ee2d8bb3d392d602411f8a595a1f021ea7b0336d
JorgeSL20 <20201175@uthh.edu.mx>
2024-07-16 22:26:37 -0600

    definitiva

--- src/app/login/interfaces/producto.interface.ts
+++ src/app/login/interfaces/producto.interface.ts
@@ -1,7 +1,7 @@
 // src/app/interfaces/producto.interface.ts
 export interface Producto {
   id: number;
-  url: string;
+  url: string; // Esta propiedad representa la URL de la imagen del producto
   producto: string;
   categoria: string;
   marca: string;
