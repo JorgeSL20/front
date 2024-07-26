8e4edef728a08077d3e23a3cd97e7c38f65df174
KitoS2001 <20200722@uthh.edu.mx>
2024-07-26 00:32:26 -0600

    ultimo

--- src/app/login/admon/headeradmon/headeradmon.component.ts
+++ src/app/login/admon/headeradmon/headeradmon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DataUser } from '../../interfaces/dataUser.interface';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-headeradmon',
@@ -8,32 +9,53 @@ import { DataUser } from '../../interfaces/dataUser.interface';
   styleUrls: ['./headeradmon.component.css']
 })
 export class HeaderadmonComponent implements OnInit {
-  dataUser: DataUser = {
-    name: "",
-    lastNameP: "",
-    lastNameM: "",
-    email: "",
-    pregunta: "",
-    respuesta:"",
-  };
-  constructor(private router: Router, private route: ActivatedRoute) { }
-
+  
   menuVariable: boolean = false;
-  menu_icon_variable: boolean = false;
+  isLoggedIn: boolean = false; // Variable para almacenar el estado de autenticación
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private loginService: LoginService // Inyecta el servicio LoginService
+  ) { }
+
+  showAlert(message: string, alertClass: string) {
+    // Crea un div para el mensaje
+    const alertDiv = document.createElement('div');
+    alertDiv.className = `alert ${alertClass} fixed-top d-flex align-items-center justify-content-center`;
+    alertDiv.textContent = message;
+    alertDiv.style.fontSize = '20px'; // Cambia el tamaño del texto
+
+    // Agrega el mensaje al cuerpo del documento
+    document.body.appendChild(alertDiv);
+
+    // Elimina el mensaje después de unos segundos
+    setTimeout(() => {
+      alertDiv.remove();
+    }, 2000);
+  }
 
   openMenu() {
     this.menuVariable = !this.menuVariable;
-    this.menu_icon_variable = !this.menu_icon_variable;
   }
 
+  checkLoggedIn() {
+    this.loginService.isLoggedIn().subscribe((loggedIn: boolean) => {
+      this.isLoggedIn = loggedIn; 
+    });
+  }
 
-  ////////////////////////////////
+  ngOnInit() {
+    this.checkLoggedIn();
+  }
+
   scrollToSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   }
+
   navigateToSection(sectionId: string): void {
     this.router.navigate([], {
       fragment: sectionId,
@@ -43,18 +65,16 @@ export class HeaderadmonComponent implements OnInit {
     this.scrollToSection(sectionId);
   }
 
-  ngOnInit() {
-
-  }
   navegar() {
-    this.router.navigate(['/'])
-
+    this.router.navigate(['/user/login']);
   }
 
-  cerrarSesion(){
-    localStorage.clear()
-    this.router.navigate(['/'])
+  cerrarSesion() {
+    this.loginService.logout(); 
+    this.checkLoggedIn();
+    localStorage.clear();
+    this.router.navigate(['/user/login']);
+    this.showAlert('Sesión cerrada con éxito', 'alert-success');
   }
-
 
 }
