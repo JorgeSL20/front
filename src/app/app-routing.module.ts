746bdbccca6af4fb5c4b952b0e4c8caeb627b50f
KitoS2001 <20200722@uthh.edu.mx>
2024-10-29 11:55:37 -0600

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,13 @@ import { NotFoundComponent } from './login/interfaces/not-found/not-found.compon
 import { CrearCuentaComponent } from './login/pages/crear-cuenta/crear-cuenta.component'
 import { RecuperarPasswordComponent } from './login/pages/recuperar-password/recuperar-password.component';
 import { RoleGuard } from './login/guards/role.guard';
+import { ContactoComponent } from './login/interfaces/contacto/contacto.component';
+import { UbicacionComponent } from './login/interfaces/ubicacion/ubicacion.component';
+import { QuienessomosComponent } from './login/interfaces/quienessomos/quienessomos.component';
+import { AvisoPrivacidadComponent } from './login/interfaces/avisoPrivacidad/avisoPrivacidad.component';
+import { TerminosCondicionesComponent } from './login/interfaces/terminosCondiciones/terminosCondiciones.component';
+import { PoliticaCookiesComponent } from './login/interfaces/politicaCookies/politicaCookies.component';
+import { PreguntasComponent } from './login/interfaces/preguntas/preguntas.component';
 
 const routes: Routes = [
   {
@@ -16,6 +23,18 @@ const routes: Routes = [
     path: 'recuperar-password',
     component: RecuperarPasswordComponent
   },
+  { 
+    path: 'recuperar-password', 
+    component: RecuperarPasswordComponent 
+  },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'ubicacion', component: UbicacionComponent },
+  { path: 'quienessomos', component: QuienessomosComponent },
+  { path: 'avisoprivacidad', component: AvisoPrivacidadComponent },
+  { path: 'terminoscondiciones', component: TerminosCondicionesComponent },
+  { path: 'politicacookies', component: PoliticaCookiesComponent },
+  { path: 'preguntas', component: PreguntasComponent },
+  
   {
     path: 'login',
     component: LoginComponent
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
-import { CrearCuentaComponent } from './pages/crear-cuenta/crear-cuenta.component';
-import { RecuperarPasswordComponent } from './pages/recuperar-password/recuperar-password.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { RecomendacionesComponent } from './pages/recomendaciones/recomendaciones.component';
 import { HeaderComponent } from './interfaces/header/header.component';
 import { FooterComponent } from './interfaces/footer/footer.component';
 import { ContactoComponent } from './interfaces/contacto/contacto.component';
-import { UbicacionComponent } from './interfaces/ubicacion/ubicacion.component';
-import { QuienessomosComponent } from './interfaces/quienessomos/quienessomos.component';
-import { AvisoPrivacidadComponent } from './interfaces/avisoPrivacidad/avisoPrivacidad.component';
-import { TerminosCondicionesComponent } from './interfaces/terminosCondiciones/terminosCondiciones.component';
-import { PoliticaCookiesComponent } from './interfaces/politicaCookies/politicaCookies.component';
 import { NotFoundComponent } from './interfaces/not-found/not-found.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { EditarPerfilComponent } from './pages/editar-perfil/editar-perfil.component';
@@ -36,15 +29,7 @@ const routes: Routes = [
       { path: 'detalles', component: ProductoDetallesComponent },
       { path: 'header', component: HeaderComponent },
       { path: 'footer', component: FooterComponent },
-      { path: 'contacto', component: ContactoComponent },
       { path: 'productospublicos', component: ServiciospublicoComponent },
-      { path: 'ubicacion', component: UbicacionComponent },
-      { path: 'registro', component: CrearCuentaComponent },
-      { path: 'recuperar-password', component: RecuperarPasswordComponent },
-      { path: 'quienessomos', component: QuienessomosComponent },
-      { path: 'avisoprivacidad', component: AvisoPrivacidadComponent },
-      { path: 'terminoscondiciones', component: TerminosCondicionesComponent },
-      { path: 'politicacookies', component: PoliticaCookiesComponent },
       { path: 'recomendaciones', component:RecomendacionesComponent},
       { path: 'preguntas', component: PreguntasComponent },
       { path: 'perfil', component: PerfilComponent },
