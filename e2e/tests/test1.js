49e8bb868d24de0b609fb8929cd4066ccda5c6c0
KitoS2001 <20200722@uthh.edu.mx>
2024-10-29 13:49:12 -0600

    Prueba

--- e2e/tests/test1.js
+++ e2e/tests/test1.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 // Configura el fixture y la página inicial
 fixture `Verificación de URL`
-    .page `https://clincicadental.netlify.app/#/`; // Cambia esta URL según sea necesario
+    .page `https://gateway-soluciones.netlify.app/#/`; // Cambia esta URL según sea necesario
 
 // Define un test
 test('Verifica que la página carga correctamente', async t => {
