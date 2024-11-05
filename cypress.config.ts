3c1a8e43bb3d970dca9941245cd712a2a59fb18f
KitoS2001 <20200722@uthh.edu.mx>
2024-11-05 17:32:52 -0600

    Pruebas

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/home.spec.ts',
     baseUrl: 'https://gateway-soluciones.netlify.app/#/', // Cambia esto a la URL de tu aplicación en el entorno de prueba
   },
 })
