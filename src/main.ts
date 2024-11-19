1eb79b5168e10e8a5b2bf90550e08115283d26c5
KitoS2001 <20200722@uthh.edu.mx>
2024-11-18 23:59:44 -0600

    Notificacion

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      navigator.serviceWorker.register('service-worker.js').then((registration) => {
         console.log('Service Worker registrado:', registration);
       }).catch((error) => {
         console.error('Error al registrar el Service Worker:', error);
