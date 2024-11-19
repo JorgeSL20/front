ecb3ca1f5193eed13af214828392f6038b229abf
KitoS2001 <20200722@uthh.edu.mx>
2024-11-19 00:22:47 -0600

    Notificacion

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,23 +7,28 @@ self.addEventListener('install', (event) => {
     console.log('Service Worker activado');
     event.waitUntil(self.clients.claim());
   
-    // Envía la notificación con un breve retraso
-    setTimeout(() => {
-      showNotification();
-    }, 1000);
+    // Enviar notificación de prueba al activar el Service Worker
+    showNotification();
   });
   
   // Función para enviar una notificación
   function showNotification() {
-    if (Notification.permission === "granted") {
-      self.registration.showNotification("¡Tienes una cita pendiente!", {
-        body: "Agenda una cita o mira nuestros servicios",
-        icon: '/assets/logo.png', // Agrega un ícono si es necesario
-      }).catch(error => {
-        console.error("Error al mostrar la notificación:", error);
-      });
-    } else {
-      console.error("No se puede mostrar la notificación: permisos denegados.");
-    }
+    self.registration.showNotification("¡Tienes una cita pendiente!", {
+      body: "Agenda una cita o mira nuestros servicios",
+      icon: './assets/logo.png'
+    }).catch(error => {
+      console.error("Error al mostrar la notificación:", error);
+    });
   }
   
+  // Asegúrate de manejar los mensajes correctamente
+  self.addEventListener('message', (event) => {
+    console.log('Mensaje recibido:', event.data);
+    event.waitUntil(
+      self.clients.matchAll().then((clients) => {
+        clients.forEach(client => {
+          client.postMessage({ message: 'Respuesta desde el Service Worker' });
+        });
+      })
+    );
+  });
