6f58e0cd970f6bc56a8ba195b47202a8a79df3cf
KitoS2001 <20200722@uthh.edu.mx>
2024-11-19 02:52:01 -0600

    Notificacion

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,4 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-// Registro del Service Worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(registration => {
-      console.log('Service Worker registrado:', registration);
 
-      // Solicitar permisos de notificación
-      if (Notification.permission === 'granted') {
-        registration.showNotification('¡Bienvenido!', {
-          body: 'Nos alegra verte de nuevo.',
-          icon: './assets/logo.png'
-        });
-      }
-    })
-    .catch(error => {
-      console.error('Error al registrar el Service Worker:', error);
-    });
-}
