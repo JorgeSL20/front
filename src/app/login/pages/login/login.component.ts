cc6d5e56f51e73803bf3370c5c3ed004b531f30b
KitoS2001 <20200722@uthh.edu.mx>
2024-12-18 04:29:09 -0600

    Monitoreo

--- src/app/login/pages/login/login.component.ts
+++ src/app/login/pages/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent {
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
   passwordVisible: boolean = false;
   logosinfondo: string = "assets/images/logosinfondo.png";
-  siteKey = '6Ld9vlUpAAAAAIBxg_WAyAL3v782D0Sv_HefWBjy';
+  siteKey = '6LevSZ8qAAAAAMxodna7btRr7IQ5fJbmCrc5WWrW';
   validRecatcha: boolean = true;
   myForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
